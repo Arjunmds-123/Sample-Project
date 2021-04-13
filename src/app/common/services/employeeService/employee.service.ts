230ad24ffc48a13874ea67ab58faa73bfa794070
arjun.porwal <arjun@metadesignsolutions.co.uk>
2021-04-13 15:02:22 +0530

    Added new comments

--- src/app/common/services/employeeService/employee.service.ts
+++ src/app/common/services/employeeService/employee.service.ts
@@ -8,22 +8,27 @@ export class EmployeeService {
 
   constructor(private http: HttpClient) { }
 
+  //get all employees data
   getAllEmployees() {
     return this.http.get("http://dummy.restapiexample.com/api/v1/employees");
   }
 
+  //get employee data by id
   getEmployeeDataById(id: any) {
     return this.http.get("http://dummy.restapiexample.com/api/v1/employee/" + id);
   }
 
+  //create a new employee
   createNewEmployee(params: any) {
     return this.http.post("http://dummy.restapiexample.com/api/v1/create", params);
   }
 
+  //update employee data
   updateEmployeeData(params: any) {
     return this.http.put("http://dummy.restapiexample.com/api/v1/update/" + params.id, params);
   }
 
+  //delete employee data
   deleteEmployee(id: any){
     return this.http.delete("http://dummy.restapiexample.com/api/v1/delete/" + id);
   }
